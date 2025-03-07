9e9bf10c182248cd16460120c5eaab52206b3516
Hensley Ferrari <hensley_ferrari@hotmail.com>
2025-03-06 21:12:57 -0300

    feat: add create method to recados controller for handling POST requests

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,7 +1,16 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
+  @HttpCode(HttpStatus.OK)
   @Get()
   findAll() {
     return `Essa rota retorna todos os recados`;
@@ -10,5 +19,10 @@ export class RecadosController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return `Essa rota retorna o recado ID ${id}`;
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return body;
   }
 }
