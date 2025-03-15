ae60372f6a6c11e8b63039c5e78c7c7d2121f922
Hensley Ferrari <hensley_ferrari@hotmail.com>
2025-03-14 22:12:20 -0300

    feat: add update and delete methods to recados controller

--- src/recados/recados.controller.ts
+++ src/recados/recados.controller.ts
@@ -1,10 +1,12 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
 } from '@nestjs/common';
 
@@ -24,5 +26,18 @@ export class RecadosController {
   @Post()
   create(@Body() body: any) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return {
+      id,
+      ...body,
+    };
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `Recado ${id} deletado com sucesso`;
   }
 }
